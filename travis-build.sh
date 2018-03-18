dcdf9b6290263a616f4195117cbb9541335f632d
Magnus Boye <magnusboye@gmail.com>
2018-03-18 23:38:54 +0200

    ci: Enable PR builds

---
+++ travis-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+VERSION=$(git describe)
+
+build_image() {
+    docker build --build-arg APP_VERSION="$VERSION" -t "$DOCKER_USERNAME/energy-logger-ui:$VERSION" .
+}
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    # PR build
+    build_image
+else
+    # Release build
+    build_image
+    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+    docker push "$DOCKER_USERNAME/energy-logger-ui:$VERSION"
+fi
